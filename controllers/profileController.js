33c539ef34f97b1d6814ae1bb6c5f0395c7195c1
Yadier <yadierg7@gmail.com>
2024-01-16 14:28:47 -0400

    updated controller profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -25,26 +25,32 @@ const validateBody = (schema) => (req, res, next) => {
     res.status(400).send({ error: error.message });
   }
 };
+const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    throw new Error("Invalid token");
+  }
+};
 
+const createProfileData = (req, decoded) => {
+  const { firstName, lastName, email, links } = req.body;
+  const profileData = {
+    userId: decoded.userId,
+    ...(firstName && { firstName }),
+    ...(lastName && { lastName }),
+    ...(email && { email }),
+    ...(links && { links }),
+  };
+
+  return profileData;
+};
 export const createProfile = [
   validateBody(createProfileSchema),
   async (req, res) => {
     try {
-      let decoded;
-      try {
-        decoded = jwt.verify(req.body.token, process.env.JWT_SECRET);
-      } catch (error) {
-        res.status(400).send({ error: "Invalid token" });
-        return;
-      }
-
-      const profileData = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        userId: decoded.userId,
-        links: req.body.links,
-      };
+      const decoded = verifyToken(req.body.token);
+      const profileData = createProfileData(req, decoded);
 
       const profile = await Profile.findOneAndUpdate(
         { userId: decoded.userId },
@@ -63,7 +69,6 @@ export const createProfile = [
     }
   },
 ];
-
 export const getProfiles = async (req, res) => {
   const { name, token } = req.query;
 
