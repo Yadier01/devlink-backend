518b8593cd39edae4e39aa84d7042cfbd4f48855
Yadier <yadier@gmail.com>
2024-01-20 13:57:42 -0400

    updated

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ const app = express();
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB", err));
-
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err);
+    process.exit(1); // Exit the process with a failure code
+  });
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoute);
 app.use("/", profileRoute);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
-  console.log("Server started on port 3000");
+  console.log(`Server started on port ${port}`);
 });
